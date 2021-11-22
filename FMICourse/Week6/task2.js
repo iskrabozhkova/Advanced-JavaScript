affaa579baabbc0017240eb2fab99bc204dd5651
iskrabozhkova <52824905+iskrabozhkova@users.noreply.github.com>
2021-11-22 09:08:17 +0200

    commit

--- FMICourse/Week6/task2.js
+++ FMICourse/Week6/task2.js
@@ -1,17 +1,32 @@
 const http = require('http');
-const { Stream } = require('stream');
+const stream = require('stream');
 
-class RevisionStream extends Stream.Transform{
+class RevisionStream extends stream.Transform{
     constructor(options){
-        
-        this.prevChunk = '';
+      super(options);
     }
-    _transform(chunk, encoding,next){
-        chunk.replace(/\b(have|think)\b/, '****')
-    }
+    prevLastPiece = '';
+   _transform(chunk, encoding="utf-8", next){
+     chunk = this.prevLastPiece + chunk;
+     const changedChunk = this._changeChunk(chunk);
+     this.push(changedChunk);
+      next();
+   }
+   _changeChunk(chunk){
+     const changedChunk =  chunk.replace(/\bhave\b/gm, '****');
+     this.prevLastPiece= changedChunk?.split(/\s+/).slice(-1)[0];
+     return changedChunk.slice(0,changedChunk.length - this.prevLastPiece.length);
+     
+   }
 }
-http.get({hostname: 'www.paulgraham.com', path: '/hwh.html',method: 'get'}, (response) => {
-    response.on('data', (data) => {
-        process.stdout.write(data);
-    });
+  
+const server = http.createServer((req, res) => {
+    http.get({hostname: 'www.paulgraham.com', path: '/hwh.html',method: 'get'}, (response) => {
+        const re = new RevisionStream();
+        response.pipe(re).pipe(res);
+    })
+})
+
+server.listen(8080, () => {
+    console.log('SERVER IS LISTENING');
 })
