1a990712489fbec4a490604f8838016291726ef3
iskrabozhkova <52824905+iskrabozhkova@users.noreply.github.com>
2021-11-29 18:40:37 +0200

    commit

--- Week8/Express/index.js
+++ Week8/Express/index.js
@@ -2,14 +2,23 @@ const express = require('express');
 const utils = require('./utils')
 const app = express();
 
+app.use(express.static('public'));
+app.use(express.urlencoded());
 app.set('view engine', 'ejs');
-app.set('layout', './layout')
+app.set('layout', './layout');
+
 
 app.get('/',(req,res)=>{
     utils.renderWithLayout(res, 'home', {results: {a: 1, b: 2}})
 })
 app.get('/about', (req,res) => {
     utils.renderWithLayout(res, 'about', {})
+})
+app.get('/login', (req,res) => {
+    utils.renderWithLayout(res, 'login', {})
+})
+app.post('/login', (req,res) => {
+    const {username,password} = req.body;
 })
 
 app.listen(8080, () => {
