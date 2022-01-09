15e96ddc500449f92db4785430a89629303622da
iskrabozhkova <52824905+iskrabozhkova@users.noreply.github.com>
2022-01-09 13:59:40 +0200

    webpack-lecture

---
+++ Week12/src/index.ts
@@ -0,0 +1,17 @@
+import {LitElement, html, css} from 'lit';
+import {customElement, property} from 'lit/decorators.js'
+
+@customElement('simple-greeting')
+export class SimpleGreeting extends LitElement{
+    static styles = css`
+    :host{
+        color: blue;
+    }
+    `;
+    @property()
+    name?: string = 'World'
+
+    render(){
+        return html`<p>Hello, ${this.name}!</p>`
+    }
+}
---
+++ Week12/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = (env) => {
+    const isProd = !!env.production;
+    const mode = isProd ? 'production' : 'development';
+    return {
+        mode,
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'app.bundle.js'
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    devtool: isProd ? undefined : 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                loader: 'ts-loader',
+                options: {
+                    configFile: path.resolve(__dirname, 'tsconfig.json')
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: './index.html'
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, dist)
+        },
+        compress: true,
+        port: 9000
+    }
+}
+}
